391f9a7ef42335ea277daa439b14f2feba25d04a
BBAER <yeybreya@gmail.com>
2025-01-29 12:09:04 +0700

    LAB15_05.cpp

--- lab15_5.cpp
+++ lab15_5.cpp
@@ -20,3 +20,39 @@ int main(){
 
 //Do not modify the code above this line
 //Write definition of shuffle() using pointer after this line. 
+void shuffle(int *a, int *b, int *c, int *d) {
+    int temp, randomIndex1, randomIndex2;
+    
+    randomIndex1 = rand() % 4;
+    randomIndex2 = rand() % 4;
+    
+    while(randomIndex1 == randomIndex2) {
+        randomIndex2 = rand() % 4;
+    }
+    
+    if(randomIndex1 == 0 && randomIndex2 == 1) {
+        temp = *a;
+        *a = *b;
+        *b = temp;
+    } else if(randomIndex1 == 0 && randomIndex2 == 2) {
+        temp = *a;
+        *a = *c;
+        *c = temp;
+    } else if(randomIndex1 == 0 && randomIndex2 == 3) {
+        temp = *a;
+        *a = *d;
+        *d = temp;
+    } else if(randomIndex1 == 1 && randomIndex2 == 2) {
+        temp = *b;
+        *b = *c;
+        *c = temp;
+    } else if(randomIndex1 == 1 && randomIndex2 == 3) {
+        temp = *b;
+        *b = *d;
+        *d = temp;
+    } else if(randomIndex1 == 2 && randomIndex2 == 3) {
+        temp = *c;
+        *c = *d;
+        *d = temp;
+    }
+}
